f426747a1a8ada0150d3d263f1b9e8b4786bd9fe
jahan <odzahan8@yandex.ru>
2021-05-01 16:08:33 +0300

    ADDED: User class and .txt file

---
+++ src/bsu/rfct/java/ovezova/laba_7/User.java
@@ -0,0 +1,21 @@
+package bsu.rfct.java.ovezova.laba_7;
+
+
+import java.util.ArrayList;
+
+public class User {
+    // private ArrayList<User> UserInfo = new ArrayList<>(10);
+    private String name;
+    private String addres;
+    public User (String name, String addres) {
+        this.name = name;
+        this.addres = addres;
+    }
+    public String getName() {
+        return name;
+    }
+    // public ArrayList<User> getUsers() { return UserInfo; }
+    public String getAddres(){
+        return addres;
+    }
+}
